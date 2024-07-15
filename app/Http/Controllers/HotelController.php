b6efad89f4fbfc6c748acef3e47b22aaf3f2441a
AsepFarhanPratama <farhanpratama6815@gmail.com>
2024-07-15 22:55:30 +0700

    membuat read hotel

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hotel;
 use Illuminate\Http\Request;
 
 class HotelController extends Controller
@@ -11,7 +12,8 @@ class HotelController extends Controller
      */
     public function index()
     {
-        //
+        $hotels = Hotel::all();
+        return view('hotels.index', compact('hotels'));
     }
 
     /**
--- resources/views/hotels/index.blade.php
+++ resources/views/hotels/index.blade.php
@@ -1,3 +1,43 @@
-<div>
-<h1>ini adalah halaman hotel</h1>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Daftar Hotel</h1>
+
+    <a href="{{ route('hotels.create') }}" class="btn btn-primary">Tambah Hotel Baru</a>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nama Hotel</th>
+                <th>Alamat</th>
+                <th>Kota</th>
+                <th>Bintang</th>
+                <th>Deskripsi</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($hotels as $hotel)
+            <tr>
+                <td>{{ $hotel->nama_hotel }}</td>
+                <td>{{ $hotel->alamat }}</td>
+                <td>{{ $hotel->kota }}</td>
+                <td>{{ $hotel->bintang }}</td>
+                <td>{{ $hotel->deskripsi }}</td>
+                <td>
+                    <a href="{{ route('hotels.show', $hotel->id) }}" class="btn btn-info btn-sm">Detail</a>
+                    <a href="{{ route('hotels.edit', $hotel->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                    <form action="{{ route('hotels.destroy', $hotel->id) }}" method="POST" style="display: inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm"
+                            onclick="return confirm('Apakah Anda yakin ingin menghapus hotel ini?')">Hapus</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
+@endsection
